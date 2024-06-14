d986681ed9cae39714d883a0766c003eca1b4552
Luke Kalyan Brata Sarker <dhrubo.luke@gmail.com>
2024-06-14 14:02:39 +0600

    ProfileController is created with index function

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Psr7\Response;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index(Response $response) {
+        
+    }
+}

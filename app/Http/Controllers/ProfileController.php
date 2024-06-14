391ddfc0b6eb1ab0ad25be782a11a77b899d5d4d
Luke Kalyan Brata Sarker <dhrubo.luke@gmail.com>
2024-06-14 14:30:24 +0600

    Data Request accept and cookie response sent

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,6 +7,27 @@ use Illuminate\Http\Request;
 class ProfileController extends Controller
 {
     public function index(Request $request) {
-        
+        // Declare variables
+        $name = "Donal Trump" ;
+        $age = "75";
+
+        //three variables in $data variable as associative array with values
+        $data = [
+                    "id"=>$request->id,
+                    "name"=>$name,
+                    "age"=>$age
+               ];
+
+        //Set cookie variables
+        $name = "access_token";
+        $value = "123-XYZ";
+        $minutes = 1;
+        $path = "/";
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure=false;
+        $httpOnly=true;
+
+        //the return statement 
+        return response($data)->cookie($name, $value,$minutes,$path,$domain,$secure,$httpOnly); 
     }
 }
